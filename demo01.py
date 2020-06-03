976a95bb9afbe475005d94718d12a48b96c7522c
uzi20200603 <736833661@qq.com>
2020-06-03 18:03:22 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,33 @@
+"""
+
+print(2333)
+print(2.333)
+print(True)
+print(())
+print([])
+print({})
+
+我叼你妈的真就白给呗
+
+name = "茄子"
+print(name)
+
+
+
+
+
+
+
+a = float(input("请输入:"))
+b = float(input("请输入:"))
+print("input获取的内容:",a+b)
+"""
+print("wgybyubuybbyubyyu","dadadadadadadada")
+a ="wgybyubuybbyubyyu"
+b ="dadadadadadadada"
+print(len(a))
+print(len(b))
+print((len(a))+(len(b)))
+
+
+
